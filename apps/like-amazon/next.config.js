3a16ce7248619177af1e6d732c3451f7be445526
kotaro_sakae <ksakae1216@gmail.com>
2024-08-04 14:55:27 +0900

    feat: create products

--- apps/like-amazon/next.config.js
+++ apps/like-amazon/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
 };
 
 const plugins = [
---
+++ apps/like-amazon/src/app/components/ProductCard.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface ProductProps {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+const ProductCard: React.FC<ProductProps> = ({ name, price, image }) => {
+  return (
+    <div className="product-card">
+      <div style={{ position: 'relative', width: '150px', height: '150px' }}>
+        <Image src={image} alt={name} layout="fill" objectFit="cover" />
+      </div>
+      <h3>{name}</h3>
+      <p>¥{price.toLocaleString()}</p>
+      <button>カートに追加</button>
+    </div>
+  );
+};
+
+export default ProductCard;
